f0a1e8173ea53a22002ebaef3c253f4e62e64baa
Ozan Tosun <102822219+ozan2sun@users.noreply.github.com>
2023-11-09 12:05:36 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using ETicaretAPI.Persistence.Contexts;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "C:\\Users\\Ozan\\OneDrive\\Masaüstü\\ETicaretUygulamasi\\ETicaretAPI\\Presentation\\ETicaretAPI.API"));
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
